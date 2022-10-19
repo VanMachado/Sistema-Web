43c10d033ca9f650a35e8b756a0f08f3b123b28b
VanMachado <vandamme.machado@gmail.com>
2022-10-19 15:58:45 -0300

    SellersController

---
+++ SalesWeb Mvc/SalesWeb Mvc/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWeb_Mvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
