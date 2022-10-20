22727dda36c352dffbcbf9bbb1867fe4c47a315c
VanMachado <vandamme.machado@gmail.com>
2022-10-20 16:37:54 -0300

    Deletar vendedor

--- SalesWeb Mvc/SalesWeb Mvc/Services/SellerService.cs
+++ SalesWeb Mvc/SalesWeb Mvc/Services/SellerService.cs
@@ -22,5 +22,18 @@ namespace SalesWeb_Mvc.Services
             _context.Seller.Add(obj);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller
+                .FirstOrDefault(obj => obj.Id == id);                            
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
     }
 }
