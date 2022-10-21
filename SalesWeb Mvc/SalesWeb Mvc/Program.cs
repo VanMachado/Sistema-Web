cacc5d0fc34a4ee1ce2854dbf2d34fd086e662e3
VanMachado <vandamme.machado@gmail.com>
2022-10-21 15:34:55 -0300

    Localização e formatação de números e datas

--- SalesWeb Mvc/SalesWeb Mvc/Program.cs
+++ SalesWeb Mvc/SalesWeb Mvc/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Globalization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Localization;
 using SalesWeb_Mvc.Data;
 using SalesWeb_Mvc.Services;
 
@@ -10,8 +12,9 @@ namespace SalesWebMvc
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            //Estabelece o banco de dados usado
             builder.Services.AddDbContext<SalesWeb_MvcContext>(options =>
-                options.UseMySql(builder.Configuration.GetConnectionString("SalesWeb_MvcContext"), 
+                options.UseMySql(builder.Configuration.GetConnectionString("SalesWeb_MvcContext"),
                     ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWeb_MvcContext")),
                         builder => builder.MigrationsAssembly("SalesWebMvc")));
 
@@ -23,13 +26,24 @@ namespace SalesWebMvc
 
             var app = builder.Build();
 
+            var enUs = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("en-US"),
+                SupportedCultures = new List<CultureInfo> { enUs },
+                SupportedUICultures = new List<CultureInfo> { enUs }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            } else
+            }
+            else
             {
                 //Como nao tem mais a classe startup eh aqui que insere o metodo para fazer inclusão no DB
                 //usando o obejeto instanciado "app"
