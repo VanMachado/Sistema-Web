ef88c3df1e7f267beb5f835700737038f01da6a8
VanMachado <vandamme.machado@gmail.com>
2022-10-21 18:29:35 -0300

    Tratamento de exceção para delete (integridade referencial)

---
+++ SalesWeb Mvc/SalesWeb Mvc/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWeb_Mvc.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
--- SalesWeb Mvc/SalesWeb Mvc/Services/SellerService.cs
+++ SalesWeb Mvc/SalesWeb Mvc/Services/SellerService.cs
@@ -27,16 +27,23 @@ namespace SalesWeb_Mvc.Services
 
         public async Task<Seller> FindByIdAsync(int id)
         {
-            return  await _context.Seller
+            return await _context.Seller
                 .Include(obj => obj.Department)
                 .FirstOrDefaultAsync(obj => obj.Id == id);
         }
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException(e.Message);
+            }
         }
 
         public async Task UpdateAsync(Seller seller)
