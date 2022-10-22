c0a68f17deb60a17d3112311a76d77f6f2d55b8c
VanMachado <vandamme.machado@gmail.com>
2022-10-22 18:52:28 -0300

    Preparando a navegação de telas de pesquisa de vendas

---
+++ SalesWeb Mvc/SalesWeb Mvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWeb_Mvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupSearch()
+        {
+            return View();
+        }
+    }
+}
