8c9519564d43c06ac75cf25bf0826fb91b26120b
VanMachado <vandamme.machado@gmail.com>
2022-10-22 21:21:58 -0300

    Implementando busca agrupada

--- SalesWeb Mvc/SalesWeb Mvc/Controllers/SalesRecordsController.cs
+++ SalesWeb Mvc/SalesWeb Mvc/Controllers/SalesRecordsController.cs
@@ -27,16 +27,28 @@ namespace SalesWeb_Mvc.Controllers
             {
                 maxDate = DateTime.Now;
             }
-            ViewData["minDate"] = minDate.Value.ToString("dd/MM/yyyy");
-            ViewData["maxDate"] = maxDate.Value.ToString("dd/MM/yyyy");
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
 
             var viewModel = await _salesRecordService.FindByDateAsync(minDate, maxDate);            
             return View(viewModel);
         }
 
-        public IActionResult GroupSearch()
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            var viewModel = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(viewModel);
         }
     }
 }
--- SalesWeb Mvc/SalesWeb Mvc/Services/SalesRecordService.cs
+++ SalesWeb Mvc/SalesWeb Mvc/Services/SalesRecordService.cs
@@ -31,5 +31,27 @@ namespace SalesWeb_Mvc.Services
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
         }
+
+        public async Task<List<IGrouping<Departament, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            var data = await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)                
+                .ToListAsync();
+            
+            return data.GroupBy(x => x.Seller.Department)
+                    .ToList();
+        }
     }
 }
